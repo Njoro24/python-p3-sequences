a4688aecd20bb958e09d47e178e02246fbbce11f
+ <+>
2025-05-09 13:19:01 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < length:
+        next_value = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_value)
+
+    print(fib_sequence)
